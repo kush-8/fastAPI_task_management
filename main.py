a88123dbdc5e22fb73ab47a71d28dd28cfa496cb
Kushagar Sharma <kushagars08@gmail.com>
2026-07-13 18:33:52 +0530

    established db connection

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
+from src.utils.settings import settings
+from src.utils.db import get_db, Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Task Management API", description="API for managing tasks", version="1.0.0")
